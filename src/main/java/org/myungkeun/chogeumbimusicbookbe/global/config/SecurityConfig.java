d173b8e0be42b2bf0cb9c9e5487ac127b0329c34
myungkeun <audrms3568@gmail.com>
2024-06-05 22:54:12 +0900

    security, jwt config

---
+++ src/main/java/org/myungkeun/chogeumbimusicbookbe/global/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.myungkeun.chogeumbimusicbookbe.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+
+public class SecurityConfig {
+    private final String[] WHITE_LIST_URL = {
+            "/**",
+            "/api/v1/**",
+            "/api/v1/auth/**",
+    };
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll())
+                .sessionManagement(sesstion ->
+                        sesstion.sessionCreationPolicy(SessionCreationPolicy
+                                .STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
